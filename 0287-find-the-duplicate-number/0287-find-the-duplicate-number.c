cc1fbed93577bd0f27987077b0ce7c9b7c07e995
Madhav S <madhavsukumar77@gmail.com>
2026-09-12 19:07:38 +0530

    Time: 10 ms (15.02%), Space: 14.9 MB (82.68%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.c
@@ -0,0 +1,15 @@
+int findDuplicate(int* nums, int numsSize) {
+    int ptr1 = nums[0];
+    int ptr2 = nums[0];
+    do {
+        ptr1 = nums[ptr1];
+        ptr2 = nums[nums[ptr2]];
+    } while (ptr1 != ptr2);
+    ptr1 = nums[0];
+    while (ptr1 != ptr2) {
+        ptr1 = nums[ptr1];
+        ptr2 = nums[ptr2];
+    }
+
+    return ptr1;
+}
